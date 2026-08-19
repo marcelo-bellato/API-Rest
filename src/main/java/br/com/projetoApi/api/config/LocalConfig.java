f7dcc48083de81993ee90bc0b1cbf2157483cd3c
MarceloBellato <bellato.consultor@gmail.com>
2026-08-19 20:28:51 -0300

    refactor: improve LocalConfig dependency injection

--- src/main/java/br/com/projetoApi/api/config/LocalConfig.java
+++ src/main/java/br/com/projetoApi/api/config/LocalConfig.java
@@ -2,21 +2,23 @@ package br.com.projetoApi.api.config;
 
 import br.com.projetoApi.api.domain.User;
 import br.com.projetoApi.api.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Configuration
 @Profile("local")
 public class LocalConfig {
 
-    @Autowired
-    private UserRepository repository;
+    private final UserRepository repository;
 
-    @Bean
+    public LocalConfig(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostConstruct
     public void startDB() {
         User u1 = new User(null, "Marcelo", "marcelo@mail.com", "123");
         User u2 = new User(null, "Luiz", "luiz@mail.com", "123");
